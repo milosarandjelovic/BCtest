64efdde3c1e038d9c6e6be968e6435deff5e8e73
milosarandjelovic <milos.kerber@gmail.com>
2024-04-10 14:58:45 +0200

    Adding two test cases and creating selectors for all elements and text used in tests

--- cypress/e2e/roto.cy.js
+++ cypress/e2e/roto.cy.js
@@ -4,7 +4,7 @@ import selectors from '../fixtures/selectors.json';
 
 describe('Sign up page tests', () => {
   beforeEach(() => {
-    cy.visit('https://rotogrinders.com/sign-up');
+    cy.visit(selectors.webPagesURLs.signUp);
   });
 
   it('Checking if all elements are on the page', () => {
@@ -40,21 +40,23 @@ describe('Sign up page tests', () => {
   });
 
   it('Checking if cursor can be placed in input fields', () => {
+    cy.get(selectors.inputSelectors.usernameField).click();
 
-    cy.get(selectors.inputSelectors.usernameField).click()
-    
-    cy.get(selectors.inputSelectors.usernameField).should('be.focused')
+    cy.get(selectors.inputSelectors.usernameField).should('be.focused');
 
-    cy.get(selectors.inputSelectors.emailField).click()
+    cy.get(selectors.inputSelectors.emailField).click();
 
-    cy.get(selectors.inputSelectors.emailField).should('be.focused')
+    cy.get(selectors.inputSelectors.emailField).should('be.focused');
 
-    cy.get(selectors.inputSelectors.passwordField).click()
+    cy.get(selectors.inputSelectors.passwordField).click();
 
-    cy.get(selectors.inputSelectors.passwordField).should('be.focused')
+    cy.get(selectors.inputSelectors.passwordField).should('be.focused');
 
-    cy.get(selectors.inputSelectors.recaptchaCheckbox).should('not.have.attr', 'aria-checked', 'true');
-
+    cy.get(selectors.inputSelectors.recaptchaCheckbox).should(
+      'not.have.attr',
+      'aria-checked',
+      'true'
+    );
   });
 
   it('Successful sign up using Username with lowercase letters and numbers', () => {
@@ -68,9 +70,9 @@ describe('Sign up page tests', () => {
 
     cy.get(selectors.itemSelectors.createButton).click();
 
-    cy.get('article.post h1').should(
+    cy.get(selectors.welcomePage.welcomeMessageElement).should(
       'have.text',
-      'Welcome to RotoGrinders! Thanks for signing up!'
+      selectors.welcomePage.welcomeMessageText
     );
   });
 
@@ -85,9 +87,45 @@ describe('Sign up page tests', () => {
 
     cy.get(selectors.itemSelectors.createButton).click();
 
-    cy.get('div.notification.error p').should(
+    cy.get(selectors.itemSelectors.notification).should(
       'have.text',
       data.errorMessage.usernameTaken
+    );
+  });
+
+  it('Unsuccessful sign up using already used email', () => {
+    cy.get(selectors.inputSelectors.usernameField).type('username1kk23k5');
+
+    cy.get(selectors.inputSelectors.emailField).type(
+      'test111123433313ww@mnsaf.com'
+    );
+    cy.get(selectors.inputSelectors.passwordField).type('password123');
+
+    cy.get(selectors.itemSelectors.createButton).click();
+
+    cy.get(selectors.itemSelectors.notification).should(
+      'have.text',
+      data.errorMessage.emailTaken
+    );
+  });
+
+  it('Unsuccessful sign up with using already used username and password', () => {
+    cy.get(selectors.inputSelectors.usernameField).type('username111123344ww1');
+
+    cy.get(selectors.inputSelectors.emailField).type(
+      'test111123433313ww@mnsaf.com'
+    );
+
+    cy.get(selectors.inputSelectors.passwordField).type('password123');
+
+    cy.get(selectors.itemSelectors.createButton).click();
+
+    const combinedText =
+      data.errorMessage.emailTaken + data.errorMessage.usernameTaken;
+
+    cy.get(selectors.itemSelectors.notification).should(
+      'have.text',
+      combinedText
     );
   });
 
@@ -102,9 +140,9 @@ describe('Sign up page tests', () => {
 
     cy.get(selectors.itemSelectors.createButton).click();
 
-    cy.get('article.post h1').should(
+    cy.get(selectors.welcomePage.welcomeMessageElement).should(
       'have.text',
-      'Welcome to RotoGrinders! Thanks for signing up!'
+      selectors.welcomePage.welcomeMessageText
     );
   });
 
@@ -119,9 +157,9 @@ describe('Sign up page tests', () => {
 
     cy.get(selectors.itemSelectors.createButton).click();
 
-    cy.get('article.post h1').should(
+    cy.get(selectors.welcomePage.welcomeMessageElement).should(
       'have.text',
-      'Welcome to RotoGrinders! Thanks for signing up!'
+      selectors.welcomePage.welcomeMessageText
     );
   });
 
@@ -136,7 +174,7 @@ describe('Sign up page tests', () => {
 
     cy.get(selectors.itemSelectors.createButton).click();
 
-    cy.get('div.notification.error p').should(
+    cy.get(selectors.itemSelectors.notification).should(
       'have.text',
       data.errorMessage.usernameMustContainsLettersOrNumbers
     );
@@ -153,7 +191,7 @@ describe('Sign up page tests', () => {
 
     cy.get(selectors.itemSelectors.createButton).click();
 
-    cy.get('div.notification.error p').should(
+    cy.get(selectors.itemSelectors.notification).should(
       'have.text',
       data.errorMessage.usernameMustContainsLettersOrNumbers
     );
@@ -168,7 +206,7 @@ describe('Sign up page tests', () => {
 
     cy.get(selectors.itemSelectors.createButton).click();
 
-    cy.get('div.notification.error p').should(
+    cy.get(selectors.itemSelectors.notification).should(
       'have.text',
       data.errorMessage.usernameRequired
     );
@@ -181,192 +219,223 @@ describe('Sign up page tests', () => {
 
     cy.get(selectors.itemSelectors.createButton).click();
 
-    cy.get('div.notification.error p').should(
+    cy.get(selectors.itemSelectors.notification).should(
       'have.text',
       data.errorMessage.emailRequired
     );
   });
 
   it('Unsuccessful sign up using Email without username', () => {
+    cy.get(selectors.inputSelectors.usernameField).type('test1234wwwqa12');
 
-    cy.get(selectors.inputSelectors.usernameField).type('test1234wwwqa12')
+    cy.get(selectors.inputSelectors.emailField).type('@mnsaf.com');
 
-    cy.get(selectors.inputSelectors.emailField).type('@mnsaf.com')
+    cy.get(selectors.inputSelectors.passwordField).type('password123');
 
-    cy.get(selectors.inputSelectors.passwordField).type('password123')
+    cy.get(selectors.itemSelectors.createButton).click();
 
-    cy.get(selectors.itemSelectors.createButton).click()
+    cy.get(selectors.itemSelectors.notification).should(
+      'have.text',
+      data.errorMessage.emailMustBeValid
+    );
+  });
 
-    cy.get('div.notification.error p').should('have.text',data.errorMessage.emailMustBeValid)
-});
+  it('Unsuccessful sign up using Email without @', () => {
+    cy.get(selectors.inputSelectors.usernameField).type('test1234wwwq22af');
 
-it('Unsuccessful sign up using Email without @', () => {
+    cy.get(selectors.inputSelectors.emailField).type('test1f123qabc.mnsaf.com');
 
-  cy.get(selectors.inputSelectors.usernameField).type('test1234wwwq22af')
+    cy.get(selectors.inputSelectors.passwordField).type('password123');
 
-  cy.get(selectors.inputSelectors.emailField).type('test1f123qabc.mnsaf.com')
+    cy.get(selectors.itemSelectors.createButton).click();
 
-  cy.get(selectors.inputSelectors.passwordField).type('password123')
+    cy.get(selectors.itemSelectors.notification).should(
+      'have.text',
+      data.errorMessage.emailMustBeValid
+    );
+  });
 
-  cy.get(selectors.itemSelectors.createButton).click()
+  it('Unsuccessful sign up using Email without domain', () => {
+    cy.get(selectors.inputSelectors.usernameField).type('test1235wwwsqaz');
 
-  cy.get('div.notification.error p').should('have.text',data.errorMessage.emailMustBeValid)
-});
+    cy.get(selectors.inputSelectors.emailField).type('test123qabcpp@');
 
-it('Unsuccessful sign up using Email without domain', () => {
+    cy.get(selectors.inputSelectors.passwordField).type('password123');
 
-  cy.get(selectors.inputSelectors.usernameField).type('test1235wwwsqaz')
+    cy.get(selectors.itemSelectors.createButton).click();
 
-  cy.get(selectors.inputSelectors.emailField).type('test123qabcpp@')
+    cy.get(selectors.itemSelectors.notification).should(
+      'have.text',
+      data.errorMessage.emailMustBeValid
+    );
+  });
 
-  cy.get(selectors.inputSelectors.passwordField).type('password123')
+  it('Unsuccessful sign up using Password with <6 characters', () => {
+    cy.get(selectors.inputSelectors.usernameField).type('testww1236sswww1qx');
 
-  cy.get(selectors.itemSelectors.createButton).click()
+    cy.get(selectors.inputSelectors.emailField).type(
+      'fapa1t1www5rwewwwss27521@mnsaf.com'
+    );
 
-  cy.get('div.notification.error p').should('have.text',data.errorMessage.emailMustBeValid)
-});
+    cy.get(selectors.inputSelectors.passwordField).type('passw');
 
-it('Unsuccessful sign up using Password with <6 characters', () => {
+    cy.get(selectors.itemSelectors.createButton).click();
 
-  cy.get(selectors.inputSelectors.usernameField).type('testww1236sswww1qx')
+    cy.get(selectors.itemSelectors.notification).should(
+      'have.text',
+      data.errorMessage.passwordMustBeSixOrMoreCharactersLong
+    );
+  });
 
-  cy.get(selectors.inputSelectors.emailField).type('fapa1t1www5rwewwwss27521@mnsaf.com')
+  // !!!! Missing specification for password field verification so next cases can't be written and tested.
 
-  cy.get(selectors.inputSelectors.passwordField).type('passw')
+  //     it('Successful sign up using Password with only upercase letters', () => {
 
-  cy.get(selectors.itemSelectors.createButton).click()
+  //         cy.get(selectors.inputSelectors.usernameField).type('test1238www')
 
-  cy.get('div.notification.error p').should('have.text',data.errorMessage.passwordMustBeSixOrMoreCharactersLong)
-});
+  //         cy.get(selectors.inputSelectors.emailField).type('fapa17rwes27521@mnsaf.com')
 
-       // !!!! Missing specification for password field verification so next cases can't be written and tested.
+  //         cy.get(selectors.inputSelectors.passwordField).type('PASSWORD')
 
+  //         cy.get(selectors.itemSelectors.createButton).click()
 
-//     it('Successful sign up using Password with only upercase letters', () => {
+  // });
 
-//         cy.get(selectors.inputSelectors.usernameField).type('test1238www')
+  //      it('Successful sign up using Password with only lowercase letters', () => {
 
-//         cy.get(selectors.inputSelectors.emailField).type('fapa17rwes27521@mnsaf.com')
+  //         cy.get(selectors.inputSelectors.usernameField).type('test1237www')
 
-//         cy.get(selectors.inputSelectors.passwordField).type('PASSWORD')
+  //         cy.get(selectors.inputSelectors.emailField).type('fapa16rwes27521@mnsaf.com')
 
-//         cy.get(selectors.itemSelectors.createButton).click()
+  //         cy.get(selectors.inputSelectors.passwordField).type('password')
 
-// });  
+  //         cy.get(selectors.itemSelectors.createButton).click()
 
-//      it('Successful sign up using Password with only lowercase letters', () => {
+  // });
 
-    //         cy.get(selectors.inputSelectors.usernameField).type('test1237www')
-    
-    //         cy.get(selectors.inputSelectors.emailField).type('fapa16rwes27521@mnsaf.com')
-    
-    //         cy.get(selectors.inputSelectors.passwordField).type('password')
-    
-    //         cy.get(selectors.itemSelectors.createButton).click()
-    
-    // });  
+  //      it('Successful sign up using Password with numbers', () => {
 
-    //      it('Successful sign up using Password with numbers', () => {
+  //         cy.get(selectors.inputSelectors.usernameField).type('test1238www')
 
-    //         cy.get(selectors.inputSelectors.usernameField).type('test1238www')
-    
-    //         cy.get(selectors.inputSelectors.emailField).type('fapa17rwes27521@mnsaf.com')
-    
-    //         cy.get(selectors.inputSelectors.passwordField).type('1234567')
-    
-    //         cy.get(selectors.itemSelectors.createButton).click()
-    
-    // });  
+  //         cy.get(selectors.inputSelectors.emailField).type('fapa17rwes27521@mnsaf.com')
 
-        //      it('Successful sign up using Password with only special characters', () => {
+  //         cy.get(selectors.inputSelectors.passwordField).type('1234567')
 
-    //         cy.get(selectors.inputSelectors.usernameField).type('test1239www')
-    
-    //         cy.get(selectors.inputSelectors.emailField).type('fapa18rwes27521@mnsaf.com')
-    
-    //         cy.get(selectors.inputSelectors.passwordField).type('!!##%%&')
-    
-    //         cy.get(selectors.itemSelectors.createButton).click()
-    
-    // });  
+  //         cy.get(selectors.itemSelectors.createButton).click()
 
-            //      it('Successful sign up using Password with only spaces', () => {
+  // });
 
-    //         cy.get(selectors.inputSelectors.usernameField).type('test1240www')
-    
-    //         cy.get(selectors.inputSelectors.emailField).type('fapa19rwes27521@mnsaf.com')
-    
-    //         cy.get(selectors.inputSelectors.passwordField).type('       ')
-    
-    //         cy.get(selectors.itemSelectors.createButton).click()
-    
-    // }); 
+  //      it('Successful sign up using Password with only special characters', () => {
 
-    it('Unsuccessful sign up with Password empty', () => {
+  //         cy.get(selectors.inputSelectors.usernameField).type('test1239www')
 
-      cy.get(selectors.inputSelectors.usernameField).type('test1241wwwqayy')
+  //         cy.get(selectors.inputSelectors.emailField).type('fapa18rwes27521@mnsaf.com')
 
-      cy.get(selectors.inputSelectors.emailField).type('fapa1u20rwes27521@mnsaf.com')
+  //         cy.get(selectors.inputSelectors.passwordField).type('!!##%%&')
 
-      cy.get(selectors.itemSelectors.createButton).click()
+  //         cy.get(selectors.itemSelectors.createButton).click()
 
-      cy.get('div.notification.error p').contains(data.errorMessage.passwordRequired)
+  // });
+
+  //      it('Successful sign up using Password with only spaces', () => {
+
+  //         cy.get(selectors.inputSelectors.usernameField).type('test1240www')
+
+  //         cy.get(selectors.inputSelectors.emailField).type('fapa19rwes27521@mnsaf.com')
+
+  //         cy.get(selectors.inputSelectors.passwordField).type('       ')
+
+  //         cy.get(selectors.itemSelectors.createButton).click()
+
+  // });
+
+  it('Unsuccessful sign up with Password empty', () => {
+    cy.get(selectors.inputSelectors.usernameField).type('test1241wwwqayy');
+
+    cy.get(selectors.inputSelectors.emailField).type(
+      'fapa1u20rwes27521@mnsaf.com'
+    );
+
+    cy.get(selectors.itemSelectors.createButton).click();
+
+    cy.get(selectors.itemSelectors.notification).contains(
+      data.errorMessage.passwordRequired
+    );
   });
 
   it('Unsuccessful sign up with Username and Email empty', () => {
+    cy.get(selectors.inputSelectors.passwordField).type('password112');
 
-      cy.get(selectors.inputSelectors.passwordField).type('password112')
+    cy.get(selectors.itemSelectors.createButton).click();
 
-      cy.get(selectors.itemSelectors.createButton).click()
+    const combinedText =
+      data.errorMessage.usernameRequired + data.errorMessage.emailRequired;
 
-      cy.get('div.notification.error p').should('have.text',data.errorMessage.usernameAndEmailRequired)
+    cy.get(selectors.itemSelectors.notification).should(
+      'have.text',
+      data.errorMessage.combinedText
+    );
   });
 
   it('Unsuccessful sign up with Username and Password empty', () => {
+    cy.get(selectors.inputSelectors.emailField).type(
+      'fapa1o21rwes27521@mnsaf.com'
+    );
 
-      cy.get(selectors.inputSelectors.emailField).type('fapa1o21rwes27521@mnsaf.com')
+    cy.get(selectors.itemSelectors.createButton).click();
 
-      cy.get(selectors.itemSelectors.createButton).click()
+    const combinedText =
+      data.errorMessage.usernameRequired + data.errorMessage.passwordRequired;
 
-      cy.get('div.notification.error p').should('have.text',data.errorMessage.usernameAndPasswordRequired)
+    cy.get(selectors.itemSelectors.notification).should(
+      'have.text',
+      data.errorMessage.combinedText
+    );
   });
 
   it('Unsuccessful sign up with Email and Password empty', () => {
-      cy.get(selectors.inputSelectors.usernameField).type('test1233wwwqaj')
+    cy.get(selectors.inputSelectors.usernameField).type('test1233wwwqaj');
 
-      cy.get(selectors.itemSelectors.createButton).click()
+    cy.get(selectors.itemSelectors.createButton).click();
 
-      cy.get('div.notification.error p').should('have.text',data.errorMessage.emailAndPasswordRequired)
+    const combinedText =
+      data.errorMessage.emailRequired + data.errorMessage.passwordRequired;
+
+    cy.get(selectors.itemSelectors.notification).should(
+      'have.text',
+      data.errorMessage.combinedText
+    );
   });
 
   it('Unsuccessful sign up with Username, Password and Email empty', () => {
+    cy.get(selectors.itemSelectors.createButton).click();
 
-      cy.get(selectors.itemSelectors.createButton).click()
+    const combinedText =
+      data.errorMessage.usernameRequired +
+      data.errorMessage.emailRequired +
+      data.errorMessage.passwordRequired;
 
-      cy.get('div.notification.error p').should('have.text',data.errorMessage.usernameEmailAndPasswordRequired)
+    cy.get(selectors.itemSelectors.notification).should(
+      'have.text',
+      data.errorMessage.combinedText
+    );
   });
 
   it('Check Privacy policy link', () => {
+    cy.get(selectors.itemSelectors.privacyPolicyButton).click();
 
-     cy.get('div.form-footer ul li p a').click()
-
-     cy.get('div.form-footer a').should('have.text','Privacy Policy') 
+    cy.get(selectors.privacyPolicyPage.title).should(
+      'have.text',
+      selectors.privacyPolicyPage.privacyPolicyTitleText
+    );
   });
 
-   it('Check Sign in link', () => {
+  it('Check Sign in link', () => {
+    cy.get(selectors.itemSelectors.signInButton).click();
 
-      cy.get('section.page-content p:nth-child(2) a').click()
-
-      cy.get('header.module-header h1').should('have.text','Sign In') 
-   });
-
-
-
-
-
-
-
-
-
-
+    cy.get(selectors.signInPage.formTitle).should(
+      'have.text',
+      selectors.signInPage.formTitleText
+    );
+  });
 });
